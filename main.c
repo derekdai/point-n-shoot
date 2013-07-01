820ffb4a0e7791e2086c7b39b2daa194693fbb7a
Derek Dai <daiderek@gmail.com>
2013-07-01 17:24:30 +0800

    add background music downloaded from http://www.nosoapradio.us/

--- main.c
+++ main.c
@@ -1,9 +1,16 @@
 #include <gtk/gtk.h>
+#include <gst/gst.h>
 #include "point-n-shoot.h"
+#include "config.h"
 
 gint main(gint argc, gchar *args[])
 {
 	gtk_init(&argc, &args);
+	gst_init(&argc, &args);
+
+	GstElement * playbin = gst_element_factory_make("playbin", "playbin");
+	g_object_set(playbin, "uri", "file://" MUSIC_DIR "DST-1990.mp3", NULL);
+	gst_element_set_state(playbin, GST_STATE_PLAYING);
 
 	PointNShoot *pns = pns_get_default();
 	pns_run(pns);
